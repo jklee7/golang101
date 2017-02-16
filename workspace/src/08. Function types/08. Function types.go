90d4cb3cb785e578cc1011e0f63d4e34b304cc3e
Jordan Lee <jklee7@gmail.com>
2017-02-16 11:40:17 +1100

    Passing functions as parameters, declaring functions as custom types, and functions which generate functions

--- workspace/src/08. Function types/08. Function types.go
+++ workspace/src/08. Function types/08. Function types.go
@@ -29,6 +29,23 @@ func Divide(x, y int) int {
 	return x / y
 }
 
+// We can also declare a function as its own type
+type DoMath func(int, int) int
+
+// This simplifies passing it as a parameter
+func Math2(x, y int, do DoMath) int {
+	return do(x, y)
+}
+
+// You can even specify functions as a return value...
+// i.e. a function that returns functions
+
+func CreateFunction(custom int) DoMath {
+	return func(x, y int) int {
+		return x + y + custom
+	}
+}
+
 func main() {
 	// We can then pass the function we want to execute on the 2 integers
 	// as a parameter to the more general Math function
@@ -36,4 +53,13 @@ func main() {
 	fmt.Println(Math(20, 10, Subtract))
 	fmt.Println(Math(20, 10, Multiply))
 	fmt.Println(Math(20, 10, Divide))
+
+	// Using functions which take function types
+	fmt.Println(Math2(20, 10, Add))
+	fmt.Println(Math2(20, 10, Subtract))
+	fmt.Println(Math2(20, 10, Multiply))
+	fmt.Println(Math2(20, 10, Divide))
+
+	// Using the CreateFunction function to generate a function!
+	fmt.Println(Math2(20, 10, CreateFunction(100)))
 }
