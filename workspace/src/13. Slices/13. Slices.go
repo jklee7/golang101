175341ef8d7d1a1306c9d232f977536ad67acadc
Jordan Lee <jklee7@gmail.com>
2017-02-28 15:18:59 +1100

    Manipulating slices in Go

--- workspace/src/13. Slices/13. Slices.go
+++ workspace/src/13. Slices/13. Slices.go
@@ -31,18 +31,26 @@ func main() {
 	slice[1] = 22
 	slice[2] = 33
 
+	// Expanding the Slice - this has to be done using the append() method
+	slice = append(slice, 44)
+
+	// You can also append a slice with another slice by specifying sliceName...
+	slice = append(slice, slice...)
+
+	fmt.Println("Slice 1")
 	for i := range slice {
 		fmt.Println(slice[i])
 	}
 
 	// Shorthand method:
+	fmt.Println("Slice 2")
 	slice2 := []int{100, 200, 300, 400, 500}
 
 	for i := range slice2 {
 		fmt.Println(slice2[i])
 	}
 
-	fmt.Println()
+	fmt.Println("Slicing up Slices")
 	// Slices can be further 'sliced up'.
 	// Remember - a slice is just a pointer
 	// slice = slice[startingIndex:endingIndex]
@@ -52,7 +60,7 @@ func main() {
 		fmt.Println(sliced[i])
 	}
 
-	fmt.Println()
+	fmt.Println("Short-hand slicing of slices... slice2[3:]")
 	// You can also do a form of shorthand slicing:
 	// by excluding the ending index, it automatically means the last index
 	sliced = slice2[3:]
@@ -60,7 +68,7 @@ func main() {
 		fmt.Println(sliced[i])
 	}
 
-	fmt.Println()
+	fmt.Println("Short-hand slicing of slices... slice2[:3]")
 	// Similarly by excluding the start index,
 	// it automatically means the first index
 	sliced = slice2[:3]
@@ -68,4 +76,12 @@ func main() {
 		fmt.Println(sliced[i])
 	}
 
+	// Deleting from a slice - this is a little wierd.
+	// We basically recreate a slice, but skip the value we want to delete.
+	// E.g. say we want to delete the second element (element 1) from slice
+	fmt.Println("Deleting from a slice")
+	slice = append(slice[:1], slice[2:]...)
+	for i := range slice {
+		fmt.Println(slice[i])
+	}
 }
